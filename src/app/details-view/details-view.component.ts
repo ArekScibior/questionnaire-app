b8371a910d21bd9ae1159d239103ef9a19d5e70e
Ścibior Arkadiusz <ascibior@kbj.com.pl>
2020-02-04 14:34:41 +0100

    Dodanie przyciskow i odpowiedzi.

--- src/app/details-view/details-view.component.ts
+++ src/app/details-view/details-view.component.ts
@@ -9,13 +9,33 @@ import { data } from '../../assets/dataQuestionnaire.json';
 	styleUrls: ['./details-view.component.css']
 })
 export class DetailsViewComponent implements OnInit {
-	data;
-	constructor(private route: ActivatedRoute) {console.log('workign', data)}
+	data: any;
+	questions: [];
+	numberOfQuestion: number = 1;
+	buttonName: string = "Next";
+	disabledButtons: boolean = false;
+	constructor(private route: ActivatedRoute) {}
 
 	ngOnInit() {
 		this.route.paramMap.subscribe(params => {
 			this.data = data[+params.get('subjectId')];
+			this.questions = this.data.questions
 		});
+	}
+	goNextQ() {
+		this.numberOfQuestion = this.numberOfQuestion+1
+		if (this.numberOfQuestion > this.questions.length) {
+			this.numberOfQuestion = this.questions.length
+			this.disabledButtons = true;
+			console.log('exported')
+			return;
+		}
+		if (this.numberOfQuestion < this.questions.length) { this.buttonName = "Next" } else { this.buttonName = "Export to XLS"}
+		
+	}
+	goBack() {
+		this.numberOfQuestion = this.numberOfQuestion -1
+		if (this.numberOfQuestion < this.questions.length) { this.buttonName = "Next" } else { this.buttonName = "Export to XLS"}
 	}
 
 }
